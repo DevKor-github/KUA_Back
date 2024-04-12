b2852ab3152748e1c68611c35e6cb67a184d0144
Euizzang8001 <a73875323@gmail.com>
2024-04-12 18:26:31 +0900

    20240412

--- back/student/serializers.py
+++ back/student/serializers.py
@@ -4,17 +4,12 @@ from rest_framework import serializers
 
 class StudentSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username= validated_data['username'],
-            password = validated_data['password'],   
-            email = validated_data['email'],
-        )
         student = models.Student(
-            user = user,
+            user = validated_data['user'],
             name = validated_data['name'],
         )
         return student
     
     class Meta:
         model = models.Student
-        fields = ['username', 'password', 'name', 'email']
+        fields = ['user', 'name']
