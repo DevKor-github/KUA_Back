7f764c039ad86aeee602185c13728d06481dcf61
Euizzang8001 <a73875323@gmail.com>
2024-05-13 10:34:22 +0900

    240513

---
+++ KU&A/student/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+from rest_framework import urls
+
+urlpatterns = [
+    path('signup/', views.SignupView.as_view()),
+    path('login/', views.LoginView().as_view()),
+    path('permissioncodesend/', views.PermissionCodeSendView().as_view()),
+    path('permissioncheck/', views.PermissionCodeCheckView().as_view()),
+]
