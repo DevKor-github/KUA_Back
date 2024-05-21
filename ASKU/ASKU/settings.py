2b64ed4cb23e4d02e6170532da85899130e9ab1b
Joyriver-laptop <dlrkd1122@gmail.com>
2024-05-21 17:16:27 +0900

    Revert "feat: 설문 앱 추가, 게시판 관련 업데이트"
    This reverts commit cb622b336874ea9a4bd6a7e9d4c9e001b9bcc88f.

--- ASKU/ASKU/settings.py
+++ ASKU/ASKU/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-
-os.environ.get("KU&A")
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,15 +77,10 @@ WSGI_APPLICATION = 'ASKU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
--- ASKU/today_poll/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodayPollConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'today_poll'
