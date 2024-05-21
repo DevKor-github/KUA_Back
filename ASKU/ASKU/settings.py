cb622b336874ea9a4bd6a7e9d4c9e001b9bcc88f
Joyriver-laptop <dlrkd1122@gmail.com>
2024-05-21 17:11:29 +0900

    feat: 설문 앱 추가, 게시판 관련 업데이트

--- ASKU/ASKU/settings.py
+++ ASKU/ASKU/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+os.environ.get("KU&A")
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +81,15 @@ WSGI_APPLICATION = 'ASKU.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
+
 
 
 # Password validation
---
+++ ASKU/today_poll/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TodayPollConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'today_poll'
