02d1cda235e6b5f4998398e58c4750c7e7f1988f
harigaze <dlrkd1122@gmail.com>
2024-07-05 20:20:47 +0900

    feat: 브리핑 자동 업데이트 기능 완료.

--- ASKU/today_poll/apps.py
+++ ASKU/today_poll/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class TodayPollConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'today_poll'
+
+    def ready(self):
+        import today_poll.signals
