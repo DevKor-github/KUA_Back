3b5234799cb633c654dd6b31a0032518065a3791
Soobin Park <79681981+estrellalila@users.noreply.github.com>
2024-07-09 11:17:22 +0900

    강의교시 2차원배열변환완료

--- 교시및강의실 추출 코드/강의교시변환.py
+++ 교시및강의실 추출 코드/강의교시변환.py
@@ -8,22 +8,31 @@ def transform_list(value):
         return value
     
     if isinstance(parsed_value, list):
-        if all(isinstance(x, str) and '-' in x for x in parsed_value):
-            return [[int(i) for i in x.split('-')] for x in parsed_value]
-        elif all(isinstance(x, (int, float)) for x in parsed_value):
-            return [[x] for x in parsed_value]
+        transformed_value = []
+        for item in parsed_value:
+            if isinstance(item, int):
+                transformed_value.append([item])
+            elif isinstance(item, str) and '-' in item:
+                try:
+                    start, end = map(int, item.split('-'))
+                    transformed_value.append(list(range(start, end + 1)))
+                except ValueError:
+                    transformed_value.append(item)
+            else:
+                transformed_value.append(item)
+        return transformed_value
     return parsed_value
 
-def process_csv_to_excel(input_csv, output_excel):
+
+def process_csv_to_excel(input_csv, output_excel, column_to_transform):
     df = pd.read_csv(input_csv)
-    df = df.applymap(transform_list)
+    # 특정 열에만 transform_list 함수 적용
+    
+    df[column_to_transform] = df[column_to_transform].apply(transform_list)
+
     df.to_excel(output_excel, index=False)
 
 # 사용 예시
-input_csv = 'your_input_file.csv'
-output_excel = 'your_output_file.xlsx'
-process_csv_to_excel(input_csv, output_excel)
-
-# 새로 추출?
-# 숫자면 바로 2차원 ㅂ매열, 
-# 2-3이;면 .....
+input_csv = 'C:\\Users\\soobin.park\\Desktop\\뎁코깃헙\\DevKor-teamB-back\\coursetable\\csv정규식적용본\\전공_modified.csv'
+output_excel = 'C:\\Users\\soobin.park\\Desktop\\뎁코깃헙\\DevKor-teamB-back\\coursetable\\csv최종본\\전공_csv.xlsx'
+process_csv_to_excel(input_csv, output_excel, '교시')
