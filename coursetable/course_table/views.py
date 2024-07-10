b07dacc62522cdb18dd560083b9698df5de0b098
estrellalila <estrellalila@naver.com>
2024-07-10 11:12:03 +0900

    views.py

--- coursetable/course_table/views.py
+++ coursetable/course_table/views.py
@@ -1,3 +1,21 @@
+import pandas as pd
 from django.shortcuts import render
+from .models import MyModel
+from .forms import UploadFileForm
 
-# Create your views here.
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            df = pd.read_excel(file)
+            for _, row in df.iterrows():
+                MyModel.objects.create(
+                    field1=row['field1'],
+                    field2=row['field2'],
+                    field3=row['field3']
+                )
+            return render(request, 'myapp/upload_success.html')
+    else:
+        form = UploadFileForm()
+    return render(request, 'myapp/upload.html', {'form': form})
