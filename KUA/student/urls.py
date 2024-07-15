201a312b0bb704db6235d3276b42b2fde387638c
Euizzang8001 <a73875323@gmail.com>
2024-07-15 16:15:37 +0900

    fix: signup, login url 수정

--- KUA/student/urls.py
+++ KUA/student/urls.py
@@ -3,8 +3,8 @@ from . import views
 from rest_framework import urls
 
 urlpatterns = [
-    path('sign-up/', views.SignupView().as_view()),
-    path('log-in/', views.LoginView().as_view()),
+    path('signup/', views.SignupView().as_view()),
+    path('login/', views.LoginView().as_view()),
     path('send-code/', views.EmailCodeSendView().as_view()),
     path('check-code/', views.EmailCodeCheckView().as_view()),
     path('create-group/', views.CreateGroupView().as_view()),
