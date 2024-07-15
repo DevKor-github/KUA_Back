77e13612b8a7c0e739f331301a4fe286ddd1ac35
Euizzang8001 <a73875323@gmail.com>
2024-07-15 16:21:45 +0900

    style: url 스타일 수정

--- KUA/student/urls.py
+++ KUA/student/urls.py
@@ -5,7 +5,7 @@ from rest_framework import urls
 urlpatterns = [
     path('signup/', views.SignupView().as_view()),
     path('login/', views.LoginView().as_view()),
-    path('emailcodesend/', views.EmailCodeSendView().as_view()),
-    path('emailcodecheck/', views.EmailCodeCheckView().as_view()),
-    path('groupCreate/', views.CreateGroupView().as_view()),
+    path('send-code/', views.EmailCodeSendView().as_view()),
+    path('check-code/', views.EmailCodeCheckView().as_view()),
+    path('create-group/', views.CreateGroupView().as_view()),
 ]
