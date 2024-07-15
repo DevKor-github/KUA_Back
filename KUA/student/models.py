3bcdbe4786437d78e8c8493f3e41a0b252802ff4
Euizzang8001 <a73875323@gmail.com>
2024-07-15 16:14:57 +0900

    fix: 닉네임 변경 시간 default 값 timezone.now()로 수정

--- KUA/student/models.py
+++ KUA/student/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
 from django.core.exceptions import ValidationError
+from django.utils import timezone
     
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length = 10, null = False, blank = False)
-    nickname_change_time = models.DateTimeField(null = True, default='2024-07-01 12:22:44.398477+09')
+    nickname_change_time = models.DateTimeField(null = True, default=timezone.now())
     points = models.IntegerField(default = 0)
     permission_date = models.DateTimeField(null = True)
     permission_type = models.CharField(max_length = 10)
