8aa16686076280b7bde56b1e4d5c90c247bfe533
Euizzang8001 <a73875323@gmail.com>
2024-07-18 10:55:02 +0900

    feat: admin 파일에 nicknamehistory 추가

--- KUA/student/admin.py
+++ KUA/student/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 #  Register your models here.
 admin.site.register(models.Student)
+admin.site.register(models.NicknameHistory)
+admin.site.register(models.CertificationCode)
