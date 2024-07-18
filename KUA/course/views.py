8fa1ea9824cbc0ce38ae53e298c0d6112d06b1e5
harigaze <dlrkd1122@gmail.com>
2024-07-18 16:51:39 +0900

    Refactor: 인증 부분 수정

--- KUA/course/views.py
+++ KUA/course/views.py
@@ -1,32 +1,44 @@
 from rest_framework import viewsets
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import Course, Tag, Post, Comment
 from .serializers import CourseSerializer, TagSerializer, PostSerializer, CommentSerializer
 
 # 강의 전체 뷰(CRUD 포함)
+
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 # 태그 전체 뷰
+
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 # 게시글 전체 뷰
+
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 # 댓글 전체 뷰
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
 # 특정 강의에 대한 게시글을 보는 뷰
+
+
 class CoursePostListView(generics.ListAPIView):
     serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
@@ -34,8 +46,11 @@ class CoursePostListView(generics.ListAPIView):
         return Post.objects.filter(course_id=course_id)
 
 # 특정 게시글의 댓글을 보는 뷰
+
+
 class PostCommentListView(generics.ListAPIView):
     serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
@@ -43,9 +58,11 @@ class PostCommentListView(generics.ListAPIView):
         return Comment.objects.filter(post_id=post_id)
 
 # 특정 태그의 게시글 리스트를 보는 뷰
+
+
 class TagPostListView(generics.ListAPIView):
     serializer_class = PostSerializer
-    
+
     def get_queryset(self):
         tag = self.kwargs['tag']
         return Post.objects.filter(tags__name=tag)
