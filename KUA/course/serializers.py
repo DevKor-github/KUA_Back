7a09b265edc1234ee51707493c617c67e8f1fcdf
Euizzang8001 <a73875323@gmail.com>
2024-07-24 22:07:54 +0900

    refactor: timetable serializer 위치 변환

--- KUA/course/serializers.py
+++ KUA/course/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Course, Tag, Post, Comment
-
+from .models import Course, Tag, Post, Comment, TimeTable
+from django.contrib.auth.models import User
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,3 +24,29 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+class TimeTableSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        username = validated_data['username']
+        check_username =  User.objects.filter(username=username).exists()
+        course_id = validated_data['course_id']
+        year = validated_data['year']
+        semester = validated_data['semester']
+        check_course =  Course.objects.filter(course_id = course_id, year = year, semester = semester).exists()
+
+        if check_username and check_course: 
+            timetable = TimeTable(
+                username = username,
+                course_id = course_id,
+                year = year,
+                semester = semester,
+            )
+            return timetable
+        elif check_username == False:
+            return {'error': 'This User id is not in User Table.'}
+        
+        elif check_course == False:
+            return {'error': 'This Course is not in Course Table.'}
+        
+        else:
+            return {'error': 'code error'}
--- KUA/student/serializers.py
+++ KUA/student/serializers.py
@@ -2,32 +2,6 @@ from . import models
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from rest_framework.response import Response
-
-class TimeTableSerializer(serializers.ModelSerializer):
-    def create(self, validated_data):
-        username = validated_data['username']
-        check_username =  models.User.objects.filter(username=username).exists()
-        course_id = validated_data['course_id']
-        year = validated_data['year']
-        semester = validated_data['semester']
-        check_course =  models.Course.objects.filter(course_id = course_id, year = year, semester = semester).exists()
-
-        if check_username and check_course: 
-            timetable = models.TimeTable(
-                username = username,
-                course_id = course_id,
-                year = year,
-                semester = semester,
-            )
-            return timetable
-        elif check_username == False:
-            return {'error': 'This User id is not in User Table.'}
-        
-        elif check_course == False:
-            return {'error': 'This Course is not in Course Table.'}
-        
-        else:
-            return {'error': 'code error'}
         
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
