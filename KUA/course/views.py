d071e240154dc36a334c2f7df5cbac2e11e74b31
Euizzang8001 <a73875323@gmail.com>
2024-07-24 22:09:17 +0900

    refactor: timetable 관련 view 위치 수정

--- KUA/course/views.py
+++ KUA/course/views.py
@@ -4,7 +4,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from .models import Course, Tag, Post, Comment
 from student.models import Student
-from .serializers import CourseSerializer, TagSerializer, PostSerializer, CommentSerializer
+from .serializers import CourseSerializer, TagSerializer, PostSerializer, CommentSerializer, TimeTableSerializer
+from rest_framework.response import Response
 
 # 강의 전체 뷰(CRUD 포함)
 class CourseViewSet(viewsets.ModelViewSet):
@@ -71,3 +72,25 @@ class StudentCommentListView(generics.ListAPIView):
         student_id = self.kwargs['student_id']
         student = get_object_or_404(Student, id=student_id)
         return Comment.objects.filter(student=student)
+    
+class SubmitTimeTableView(generics.CreateAPIView): #id, 학수번호, 개설년도와 학기 정보를 통해 시간표를 등록하는 view
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        print(user.username)
+        course_id = request.data['course_id']
+        year = request.data['year']
+        semester = request.data['semester']
+        timetable = {
+            'username': user.username,
+            'course_id': course_id,
+            'year': year,
+            'semester': semester
+        }
+        serializer = TimeTableSerializer(data = timetable)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'response': 'Time Table Saved'})
+        return Response({'response': 'Time Table Rejected'})
--- KUA/student/views.py
+++ KUA/student/views.py
@@ -119,28 +119,6 @@ class LoginView(APIView):
             return Response({"Token": token.key})
         else:
             return Response(status = 401)
-        
-class SubmitTimeTableView(generics.CreateAPIView): #id, 학수번호, 개설년도와 학기 정보를 통해 시간표를 등록하는 view
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        user = request.user
-        print(user.username)
-        course_id = request.data['course_id']
-        year = request.data['year']
-        semester = request.data['semester']
-        timetable = {
-            'username': user.username,
-            'course_id': course_id,
-            'year': year,
-            'semester': semester
-        }
-        serializer = serializers.TimeTableSerializer(data = timetable)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'response': 'Time Table Saved'})
-        return Response({'response': 'Time Table Rejected'})
 
 class PointGetView(generics.UpdateAPIView):  # 포인트 값과 user를 받아 해당 포인트 값만큼 유저가 포인트를 얻는 view
     authentication_classes = [TokenAuthentication]
