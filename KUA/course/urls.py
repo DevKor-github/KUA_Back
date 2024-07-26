8328c31d550ba6596bc7acc82f58a4352dbc9dae
Joyriver-laptop <dlrkd1122@gmail.com>
2024-07-26 17:00:24 +0900

    Refactor: URL 수정

--- KUA/course/urls.py
+++ KUA/course/urls.py
@@ -15,31 +15,16 @@ urlpatterns = [
     path('tags/<str:tag>/posts/', views.TagPostListView.as_view(), name='tag-posts'),
     path('students/<int:student_id>/posts/', views.StudentPostListView.as_view(), name='student-posts'),
     path('students/<int:student_id>/comments/', views.StudentCommentListView.as_view(), name='student-comments'),
+    path('submit-timetable/', views.SubmitTimeTableView().as_view()),
     
-        # 특정 게시판 게시글
+    
+    # 특정 게시판 게시글
     path('courses/<int:course_id>/posts/',
          views.CoursePostListView.as_view(), name='course-posts'),
-
-    # 특정 게시판의 브리핑 목록
-    path('courses/<int:course_id>/briefings/',
-         views.CourseBriefingListView.as_view(), name='course-briefings'),
-
     # 특정 게시글의 댓글
     path('posts/<int:post_id>/comments/',
          views.PostCommentListView.as_view(), name='post-comments'),
-    
     # 특정 태그의 게시글
     path('posts/<int:tag_id>/', views.TagPostListView.as_view(), name='post-tags'),
 
-    # 특정 사용자의 오늘의 설문 목록
-    path('student/<int:user_id>/todaypolls/', views.UserTodayPollListCreateView.as_view(),
-         name='user-todaypoll-list-create'),
-    
-    # 특정 설문 디테일
-    path('todaypolls/<int:pk>/', views.TodayPollDetailView.as_view(),
-         name='todaypoll-detail'),
-    
-    # 특정 설문 응답
-    path('todaypolls/<int:pk>/answer/', views.TodayPollAnswerView.as_view(),
-         name='todaypoll-answer'),
 ]
