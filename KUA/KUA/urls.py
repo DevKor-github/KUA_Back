6dac6e2428e43f4d23ec05cdc98b9674702e62e9
Euizzang8001 <a73875323@gmail.com>
2024-07-29 16:12:16 +0900

    style: swagger 페이지 부가 설명 추가

--- KUA/KUA/urls.py
+++ KUA/KUA/urls.py
@@ -11,10 +11,19 @@ schema_view = get_schema_view(
     openapi.Info(
         title="KU&A",
         default_version='0.1a',
-        description="KU&A API 문서입니다.",
+        description="""
+            <h2>KU&A API 문서입니다</h2>
+            <p>이 문서는 KU&A API의 사용법을 설명합니다.</p>
+            <ul>
+                <li>버전: 0.1a</li>
+                <li>용어 및 서비스 약관: <a href="https://www.google.com/policies/terms/">여기</a>를 참조하세요.</li>
+                <li>문의: <a href="mailto:dlrkd1122@korea.ac.kr">dlrkd1122@korea.ac.kr</a></li>
+                <li>라이선스: DevKor</li>
+                <li>참고 사항: Token 사용 방식: 자물쇠 버튼 클릭 -> 'token <해당 유저의 token>'형식으로 입력</li>
+            </ul>
+            <p><strong>참고사항:</strong> KU&A API는 현재 개발 중입니다. 기능이 추가되거나 변경될 수 있습니다.</p>
+        """,
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="dlrkd1122@korea.ac.kr"), # 부가정보
-        license=openapi.License(name="DevKor"),     # 부가정보
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
