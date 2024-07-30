6b2accc7275cb8643405e8593c248fa849e9239b
harigaze <dlrkd1122@gmail.com>
2024-07-31 03:33:34 +0900

    Feat: 스케줄러 동작 중인지 로그 따는 기능 추가

--- KUA/KUA/settings.py
+++ KUA/KUA/settings.py
@@ -49,6 +49,37 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+# default
+APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
+
+# 자동으로 스케쥴러 실행
+SCHEDULER_DEFAULT = True
+
+# 스케줄러 로그
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'scheduler.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'apscheduler': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Application definition
 
