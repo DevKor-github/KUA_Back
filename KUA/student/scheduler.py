e9615bf8a5706c16af685cbb04910bc285275f9d
harigaze <dlrkd1122@gmail.com>
2024-07-31 03:31:51 +0900

    Refactor: 스케줄러 중복 초기화 제거

--- KUA/student/scheduler.py
+++ KUA/student/scheduler.py
@@ -6,13 +6,14 @@ from django.utils import timezone
 from datetime import datetime, timedelta
 import os
 from dotenv import load_dotenv
- 
+
 scheduler = BackgroundScheduler()
 
 load_dotenv()
 
 nickname_animal = ['사자', '고양이', '강아지', '호랑이', '매', '양', '토끼', '용', '용', '다람쥐', '돼지', '소', '쥐', '파리', '모기',
                    '까마귀', '벌', '개미', '염소', '하마', '코뿔소', '곰', '뱀', '원숭이', '고릴라', '말']
+
 
 def update_data():
     # DB 연결
@@ -21,26 +22,27 @@ def update_data():
         dbname = os.environ.get('DB_NAME')
         dbuser = os.environ.get('DB_USER')
         dbpassword = os.environ.get('DB_PASSWORD')
-        conn = psycopg2.connect(host=host, dbname=dbname, user=dbuser, password=dbpassword)
+        conn = psycopg2.connect(host=host, dbname=dbname,
+                                user=dbuser, password=dbpassword)
     except psycopg2.DatabaseError as db_err:
         print(db_err)
         return False
 
     cur = conn.cursor()
-    
-    # 랜덤 숫자 만들기 
+
+    # 랜덤 숫자 만들기
     cur.execute('SELECT id FROM student_student;')
     student_ids = cur.fetchall()
     student_number = len(student_ids)
-    
+
     if student_number == 0:
         print("No students found.")
         conn.close()
         return False
-    
+
     numbers = list(range(student_number))
     random.shuffle(numbers)
- 
+
     # 랜덤 닉네임 만들기
     random_animal = random.choices(nickname_animal, k=student_number)
     nicknames = []
@@ -60,7 +62,7 @@ def update_data():
             INSERT INTO student_nicknamehistory
             VALUES ( %s, %s, %s);
         ''', (student_id, nickname, nickname_change_time))
-    
+
     conn.commit()
     conn.close()
 
@@ -68,11 +70,10 @@ def update_data():
     return True
 
 
-scheduler = BackgroundScheduler()
-
 def start_scheduler():
     start_time = datetime(2024, 7, 4, 0, 0, 0)
     if not scheduler.running:
         # 2024년 7월 4일 오전 12시 이후 1분마다 실행되도록 설정
-        scheduler.add_job(update_data, 'interval', hours=24, start_date=start_time)
+        scheduler.add_job(update_data, 'interval',
+                          hours=24, start_date=start_time)
         scheduler.start()
