3e2db03c4fbe42bf6b0e13c0008972a62c0a7bb4
Euizzang8001 <a73875323@gmail.com>
2024-07-30 17:56:13 +0900

    fix: 실행을 위한 코드 수정

--- KUA/KUA/settings.py
+++ KUA/KUA/settings.py
@@ -47,7 +47,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
