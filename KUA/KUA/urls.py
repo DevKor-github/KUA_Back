0fa2aeebeb133791bbb3f3e7fa0f4063fb7b9795
harigaze <dlrkd1122@gmail.com>
2024-07-31 04:02:49 +0900

    Fix: STATIC_URL 삭제

--- KUA/KUA/urls.py
+++ KUA/KUA/urls.py
@@ -4,7 +4,7 @@ from . import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
-from drf_yasg       import openapi
+from drf_yasg import openapi
 from django.views.generic import RedirectView
 
 schema_view = get_schema_view(
@@ -24,7 +24,7 @@ schema_view = get_schema_view(
             <p><strong>참고사항:</strong> KU&A API는 현재 개발 중입니다. 기능이 추가되거나 변경될 수 있습니다.</p>
         """,
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="kuna.devkor@gmail.com"), # 부가정보
+        contact=openapi.Contact(email="kuna.devkor@gmail.com"),  # 부가정보
         license=openapi.License(name="DevKor"),     # 부가정보
     ),
     public=True,
@@ -32,18 +32,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
-    re_path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc-v1'),
+    re_path(r'swagger(?P<format>\.json|\.yaml)',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', RedirectView.as_view(url='/swagger', permanent=True)),
-    
+
     path('admin/', admin.site.urls),
     path('api/', include('rest_framework.urls')),
     path('student/', include('student.urls')),
     path('course-table/', include('course_table.urls')),
     path('', include('course.urls')),
-    path('', include('today_poll.urls')),   
+    path('', include('today_poll.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
-
+# if settings.DEBUG:
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
