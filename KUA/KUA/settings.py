c7878cc3c70c44a095cebf3b0172dc85e48c81d5
Euizzang8001 <a73875323@gmail.com>
2024-07-31 15:14:38 +0900

    feat: 한국 중심의 시간으로 timezone 수정

--- KUA/KUA/settings.py
+++ KUA/KUA/settings.py
@@ -204,11 +204,13 @@ REST_FRAMEWORK = {
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
