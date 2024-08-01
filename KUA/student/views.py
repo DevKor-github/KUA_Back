a7b2f79a4fb36d429b0f99f1068c362cebe2d1a6
harigaze <dlrkd1122@gmail.com>
2024-08-01 16:22:20 +0900

    Fix: user.last_login을 aware datatime으로 변환

--- KUA/student/views.py
+++ KUA/student/views.py
@@ -203,7 +203,11 @@ class LoginView(APIView):
             username=request.data['username'], password=request.data['password'])
         if user is not None:
             token = Token.objects.get(user=user)
-            user.last_login = timezone.localtime(timezone.now())
+            current_time = timezone.now()
+            if timezone.is_naive(current_time):
+                current_time = timezone.make_aware(
+                    current_time, timezone.get_current_timezone())
+            user.last_login = timezone.localtime(current_time)
             user.save()
             return Response({"Token": token.key})
         else:
