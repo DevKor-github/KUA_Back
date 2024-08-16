f8bf65b8111d331ca993c6ebb83bb1024c7005dd
Joyriver-laptop <dlrkd1122@gmail.com>
2024-08-16 17:15:23 +0900

    Refactor: 브리핑 모델이 JSON을 담을 수 있게 변경

--- KUA/today_poll/models.py
+++ KUA/today_poll/models.py
@@ -8,17 +8,19 @@ class TodayPoll(models.Model):
     check_test = models.BooleanField(default=False, null=False)
     check_homework = models.BooleanField(default=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    answered_at = models.DateTimeField(null=True, auto_now=True)
+    answered_at = models.DateTimeField(default= None)
     expired = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.course.course_name} - {self.user.username} - {self.created_at.date()}"
 
 
+
+
 class Briefing(models.Model):
     course_fk = models.ForeignKey(
         'course.Course', on_delete=models.CASCADE, related_name='briefings')
-    content = models.TextField()
+    content = models.JSONField(default=dict)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
