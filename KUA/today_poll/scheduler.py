14f22cb84b5901a99e365ad79eababee370e6ee7
Joyriver-laptop <dlrkd1122@gmail.com>
2024-08-16 17:16:26 +0900

    Feat: 설문 자동생성과 삭제 스케줄러 구현.

--- KUA/today_poll/scheduler.py
+++ KUA/today_poll/scheduler.py
@@ -1,56 +1,43 @@
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore, register_events
-from apscheduler.triggers.cron import CronTrigger
+from today_poll.models import TodayPoll
+from course.models import TimeTable
 from django.utils import timezone
-import sys
 
 logger = logging.getLogger(__name__)
+def create_today_poll():
+    try:
+        logger.info("create_today_poll 작업 시작")
+        timetables = TimeTable.objects.all()
+        
+        for timetable in timetables:
+            student = timetable.student
+            courses = timetable.courses.all()
+            for course in courses:
+                TodayPoll.objects.create(student=student, course_fk=course)
+                logger.info(f"{student.nickname}의 TodayPoll 생성: {course.course_name}")
+
+        logger.info("create_today_poll 작업 완료")
+    except Exception as e:
+        logger.error(f"create_today_poll 작업 중 예외 발생: {str(e)}")
 
 
-def test_scheduler():
-    dt_now = timezone.now()
-    logger.debug(f'dt_now: {dt_now}')
-
+def delete_old_today_poll():
+    # 현재 시각에서 하루를 뺀 시간 계산
+    threshold = timezone.now() - timezone.timedelta(days=1)
+    # 해당 시간 이전에 생성된 TodayPoll 인스턴스 삭제
+    old_polls = TodayPoll.objects.filter(created_at__lt=threshold)
+    count, _ = old_polls.delete()
+    logger.info(f"{count}개의 TodayPoll 인스턴스가 삭제되었습니다.")
 
 def start():
-    try:
-        job_id = 'job_1'
-        sched = BackgroundScheduler(timezone='Asia/Seoul')
-        sched.add_jobstore(DjangoJobStore(), "default")
-        register_events(sched)
-        sched.start()
-        logger.info("Scheduler started...")
+    scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), 'default')
 
-        sched.add_job(test_scheduler, 'cron', second='*/5',
-                      id=job_id, jobstore='default', replace_existing=True)
-        logger.info(f"Job {job_id} added.")
-    except Exception as ex:
-        logger.error(ex)
-        sched.shutdown()
+    scheduler.add_job(create_today_poll, 'cron', hour=0, minute=0, id='create_today_poll', replace_existing=True)
+    scheduler.add_job(delete_old_today_poll, 'cron', hour=0, minute=0, id='delete_old_today_poll', replace_existing=True)
 
-
-# from apscheduler.schedulers.background import BackgroundScheduler
-# from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
-# from today_poll.models import TodayPoll
-# from django.contrib.auth.models import User
-# from course.models import Course
-
-
-# def create_today_poll():
-#     default_user = User.objects.get(pk=1)
-#     courses = Course.objects.all()
-#     for course in courses:
-#         TodayPoll.objects.create(user=default_user, course=course)
-
-
-# def start():
-#     scheduler = BackgroundScheduler()
-#     scheduler.add_jobstore(DjangoJobStore(), 'default')
-
-#     # 매일 자정에 실행되도록 스케줄러 설정
-#     register_job(scheduler, 'cron', create_today_poll, hour=0, minute=0)
-
-#     register_events(scheduler)
-#     scheduler.start()
-#     print("Scheduler started")
+    register_events(scheduler)
+    scheduler.start()
+    logger.info("Scheduler started")
