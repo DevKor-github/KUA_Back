5a2bd08b74f01465ef9122fe4c90f1792660ab58
Euizzang8001 <a73875323@gmail.com>
2024-08-17 14:45:28 +0900

    fix: many to many에서 ForeignKey로 수정

--- KUA/student/models.py
+++ KUA/student/models.py
@@ -28,7 +28,7 @@ class CertificationCode(models.Model):
         return self.email
     
 class NicknameHistory(models.Model):
-    user = models.ManyToManyField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=10, null = False, blank = False)
     nickname_time = models.DateTimeField(null = False)
 
