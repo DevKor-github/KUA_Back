fc0fd289df47e9020f399c75359c9435b3246c85
harigaze <dlrkd1122@gmail.com>
2024-08-17 21:59:48 +0900

    Fix: 파일 업로드 기능 수정

--- KUA/KUA/settings.py
+++ KUA/KUA/settings.py
@@ -231,8 +231,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
