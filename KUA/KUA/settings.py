28750a09e9b0bb8090a7cee388c915ff888919c3
Joyriver-laptop <dlrkd1122@gmail.com>
2024-09-10 19:22:07 +0900

    Test: Allowed_Host에 ip 주소 추가

--- KUA/KUA/settings.py
+++ KUA/KUA/settings.py
@@ -47,7 +47,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-dS = ['*']
+ALLOWED_HOSTS = ['3.37.163.236', 'localhost', '127.0.0.1']
 
 # default
 APSCHEDULER_DATETIME_FORMAT = "N j, Y, f:s a"
