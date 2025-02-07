63ccbfd2335c838296a94e818ce4da76ee2fdc8e
yigang.jo <yigang.jo@hlcompany.com>
2025-02-07 16:27:41 +0900

    Fix: Related name 수정 후 모델 마이그레이션 되는지 체크

--- KUA/student/models.py
+++ KUA/student/models.py
@@ -64,23 +64,23 @@ class Image(models.Model):
         return f"{self.name}의 {self.tag} - 이미지"
     
     
-# # 유저 활동 기록
+# 유저 활동 기록
 
-# class StudentHistory(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     post_liked = models.ManyToManyField('course.Post', related_name='likes', blank=True)
+class StudentHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_liked = models.ManyToManyField('course.Post', related_name='history_likes', blank=True)
     
-#     post_scraped = models.ManyToManyField('course.Post', related_name='scraped', blank=True)
+    post_scraped = models.ManyToManyField('course.Post', related_name='history_scraped', blank=True)
     
-#     comment_liked = models.ManyToManyField('course.Comment', related_name='likes', blank=True)
+    comment_liked = models.ManyToManyField('course.Comment', related_name='history_likes', blank=True)
     
-#     user_blocked = models.ManyToManyField('course.User', related_name='blocked', blank=True)
+    user_blocked = models.ManyToManyField('course.User', related_name='history_blocked', blank=True)
     
-#     user_followed = models.ManyToManyField('course.User', related_name='followed', blank=True)
+    user_followed = models.ManyToManyField('course.User', related_name='history_followed', blank=True)
     
-#     post_posted = models.ManyToManyField('course.Post', related_name='posted', blank=True)
+    post_posted = models.ManyToManyField('course.Post', related_name='history_posted', blank=True)
     
-#     comment_commented = models.ManyToManyField('course.Comment', related_name='commented', blank=True)
+    comment_commented = models.ManyToManyField('course.Comment', related_name='history_commented', blank=True)
     
-#     def __str__(self):
-#         return f"{self.student}의 활동 기록"
+    def __str__(self):
+        return f"{self.student}의 활동 기록"
