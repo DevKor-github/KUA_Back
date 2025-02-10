bf7385e7e10e53c605a8d05bc326ae302f249fa3
yigang.jo <yigang.jo@hlcompany.com>
2025-02-10 10:00:18 +0900

    Fix

--- KUA/course/views.py
+++ KUA/course/views.py
@@ -426,7 +426,10 @@ class PostViewSet(viewsets.ModelViewSet):
     @swagger_auto_schema(
         operation_summary="게시글 좋아요",
         operation_description="게시글 좋아요 여부를 변경합니다.",
-        request_body=EmptySerializer,
+        manual_parameters=[
+            openapi.Parameter('post_id', openapi.IN_QUERY,
+                              description="게시글 ID", type=openapi.TYPE_INTEGER),
+        ],
         responses={200: 'Success'}
     )
     @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated], serializer_classes=EmptySerializer)
