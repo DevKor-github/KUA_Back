ff3144fd367dc63670d03fc75fbca6d8e3492082
yigang.jo <yigang.jo@hlcompany.com>
2025-02-10 10:08:07 +0900

    Fix

--- KUA/course/views.py
+++ KUA/course/views.py
@@ -426,13 +426,10 @@ class PostViewSet(viewsets.ModelViewSet):
     @swagger_auto_schema(
         operation_summary="게시글 좋아요",
         operation_description="게시글 좋아요 여부를 변경합니다.",
-        manual_parameters=[
-            openapi.Parameter('post_id', openapi.IN_QUERY,
-                              description="게시글 ID", type=openapi.TYPE_INTEGER),
-        ],
+        request_body=PostMinimalSerializer,
         responses={200: 'Success'}
     )
-    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated], serializer_classes=EmptySerializer)
+    @action(detail=True, methods=['POST'], permission_classes=[IsAuthenticated])
     def like(self, request, pk=None):
         post = self.get_object()
         user = request.user
